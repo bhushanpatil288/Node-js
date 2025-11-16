80a1215412c72f74b7c47d29be4a8e85c3b51726
bhushanpatil288 <sharewithbhushan@gmail.com>
2025-11-16 10:46:34 +0530

    [add]:nginx-server

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 8080;
+
+const server = http.createServer((req, res)=>{
+  const filePath = path.join(__dirname, req.url === '/' ? "index.html" : req.url)
+
+  const exName = String(path.extname(filePath).toLowerCase())
+
+  const mimeTypes = {
+    '.html' : 'text/html',
+    '.css' : 'text/css',
+    '.js' : 'text/javascript',
+    '.png' : 'text/png',
+  }
+
+  const contentType = mimeTypes[exName] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content)=>{
+    if(err){
+      if(err.code === 'ENOENT'){
+        res.writeHead(404, {'content-Type': "text/html"});
+        res.end('404: File not Found');
+      }
+    } else {
+      res.writeHead(200, {'content-Type': contentType});
+      res.end(content, 'utf-8');
+    }
+  });
+
+});
+
+console.log(path.join(__dirname, 'index.html'));
+
+server.listen(port, ()=>{
+  console.log(`listening on port ${port}`);
+})
